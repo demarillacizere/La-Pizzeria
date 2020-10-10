c92eb3a474df9cd13b334e76499fa48cd2c376cd
Demarillac Izere <izered3@gmail.com>
2020-10-10 23:05:09 +0200

    added a function to display toppings according to the size

--- js/scripts.js
+++ js/scripts.js
@@ -13,3 +13,25 @@ Order.prototype.ordersPrice=function(){
     sum +=this.totalPrice();
     return sum;
 }
+
+//user-interface logic
+
+$(document).ready(function(){
+    $('#new-orders').on('change','#size',function(){
+        if($('#size').val()=='1500'){
+            $('#small-toppings').show();
+            $('#medium-toppings').hide();
+            $('#large-toppings').hide();
+       }
+       else if($('#size').val()=='2000'){
+        $('#small-toppings').hide();   
+        $('#medium-toppings').show();
+        $('#large-toppings').hide();
+       }
+       else if($('#size').val()=='3000'){
+        $('#small-toppings').hide();
+        $('#medium-toppings').hide();   
+        $('#large-toppings').show();
+       }
+    });
+})
